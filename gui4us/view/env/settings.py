a56148fd90783122025ca874d27a50ea2c915c7b
Piotr Jarosik <piotr.jarosik@us4us.eu>
2023-09-04 23:43:05 +0200

    Removed non-relevant comments.

--- gui4us/view/env/settings.py
+++ gui4us/view/env/settings.py
@@ -6,27 +6,6 @@ from gui4us.model import *
 from gui4us.view.env.widgets import Slider, SpinBox, WidgetSequence
 
 
-# class SettingsPanel(pn.viewable.Viewer):
-#
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         widgets = [pn.pane.Markdown("**Voltage [V]**")]
-#         widgets.append(
-#             pn.widgets.IntInput(value=5, step=5, start=5, stop=90)
-#         )
-#         widgets.append(pn.pane.Markdown("**TGC [dB]**"))
-#         for i in range(5):
-#             slider = pn.widgets.EditableFloatSlider(
-#             name=f"{i*10} [mm]", start=14, end=54, step=0.1, value=34)
-#             widgets.append(slider)
-#         self._layout = pn.Column(
-#             *widgets,
-#             sizing_mode="stretch_both"
-#         )
-#
-#     def __panel__(self) -> pn.viewable.Viewable:
-#         return self._layout
-#
 
 class SettingsPanel(pn.viewable.Viewer):
 
